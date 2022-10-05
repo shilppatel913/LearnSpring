b86f43e6598b8d4f5b3f3abcb0b0f5283e49e820
shilp <shilppatel77@gmail.com>
2022-10-06 03:31:55 +0530

    Autowiring using xml byName,byType,constructor

---
+++ src/main/java/corespring/springcore/auto/wire/Employee.java
@@ -0,0 +1,32 @@
+package corespring.springcore.auto.wire;
+
+//Employee class is dependent on address class we can do autowiring here as we are using object of the address class.
+public class Employee {
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Employee(Address address) {
+		super();
+		this.address = address;
+	}
+	
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+
+}
