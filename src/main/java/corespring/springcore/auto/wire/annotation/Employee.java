d83be8f1671704b65938db222ff8dd2e100bc011
shilp <shilppatel77@gmail.com>
2022-10-06 14:18:09 +0530

    Removing autowiring ambiguity with the help of qualifier annotation

--- src/main/java/corespring/springcore/auto/wire/annotation/Employee.java
+++ src/main/java/corespring/springcore/auto/wire/annotation/Employee.java
@@ -1,11 +1,15 @@
 package corespring.springcore.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 //Employee class is dependent on address class we can do autowiring here as we are using object of the address class.
 public class Employee {
 	//this autowired annotation can be used above an address, a setter method and a constructor
+	//If in the configuration there are two beans with different names IOC will get confused which one to inject
+	//so we can apply qualifier annotation and give the name of that particular bean insid the args
 	@Autowired
+	@Qualifier("address2")
 	private Address address;
 
 	public Address getAddress() {
